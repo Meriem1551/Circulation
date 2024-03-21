453b911543a87787c941bf558886be68ad233948
Meriem1551 <meriem200409@gmail.com>
2024-03-21 18:50:50 +0100

    fixing some issues

--- circulation/src/main/java/com/circ/Circulation.java
+++ circulation/src/main/java/com/circ/Circulation.java
@@ -4,15 +4,16 @@ public class Circulation {
     public static void main(String[] args) {
         // Object a = new Automobile(0, 50);
         Automobile b = new Automobile(0, 50);// duplicated variable a so we change it to b
-        Object v = new Velo(0, "VTT");
+        // Object v = new Velo(0, "VTT");
         Velo ve = new Velo(0, "VTT");// duplicated variable v so we change it to ve
         Vehicule vh = new Vehicule(3, 10);
         Conducteur c = new Conducteur("Karim", "1234567", 12);// we should add a value for acceleration attribut
         ve.tombe();// tombe method is undefined for Object class so we need to change v with ve
-        c.conduit((Automobile) v);
+        c.conduit(b);// can't cast to Automobile from Velo
         c.conduit(b);// we can't acces a method unsing object from the Object class, we change a with
                      // b
-        vh = b;// can't convert from Object to Vehicule, but we can assign an Object that is a
+        vh = b;// can't convert from Object to Vehicule, but we can assign an object that is a
+               // of
                // sub class of Vehicule
         ((Automobile) vh).faitLePlein(10);
     }
